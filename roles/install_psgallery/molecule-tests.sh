4282853750f5bc1334032a709c7bf8057063bcdd
Jim Tarpley <46848743+trippsc2@users.noreply.github.com>
2024-06-29 21:17:37 -0400

    Improved tests. Added collection dependencies.

--- roles/install_psgallery/molecule-tests.sh
+++ roles/install_psgallery/molecule-tests.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-MOLECULE_BOX="w2019_cis" molecule test
 MOLECULE_BOX="w2022_cis" molecule test
-
 MOLECULE_BOX="w11_23h2_base" molecule test
+MOLECULE_BOX="w2019_cis" molecule test
 MOLECULE_BOX="w10_22h2_base" molecule test
