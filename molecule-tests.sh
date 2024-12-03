5697690025c3f8f74683c08171b22a930fa51702
TrippSC2 <46848743+trippsc2@users.noreply.github.com>
2024-12-03 11:32:57 -0500

    Added Server 2025 and Windows 11 24H2 to testing.

--- molecule-tests.sh
+++ molecule-tests.sh
@@ -6,3 +6,5 @@ MOLECULE_BOX="w2022_cis" molecule test -s win_combine_certificates
 MOLECULE_BOX="w2022_cis" molecule test -s win_package_provider
 MOLECULE_BOX="w2019_cis" molecule test -s win_combine_certificates
 MOLECULE_BOX="w2019_cis" molecule test -s win_package_provider
+MOLECULE_BOX="w2025_base" molecule test -s win_combine_certificates
+MOLECULE_BOX="w2025_base" molecule test -s win_package_provider
--- roles/install_psgallery/molecule-tests.sh
+++ roles/install_psgallery/molecule-tests.sh
@@ -3,6 +3,8 @@
 set -e
 
 MOLECULE_BOX="w2022_cis" molecule test
-MOLECULE_BOX="w11_23h2_base" molecule test
 MOLECULE_BOX="w2019_cis" molecule test
+MOLECULE_BOX="w11_24h2_base" molecule test
+MOLECULE_BOX="w2025_base" molecule test
+MOLECULE_BOX="w11_23h2_base" molecule test
 MOLECULE_BOX="w10_22h2_base" molecule test
