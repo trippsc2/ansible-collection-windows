436423a8e0b2b40f67b957230e6059259df90f95
Jim Tarpley <46848743+trippsc2@users.noreply.github.com>
2025-03-05 08:59:20 -0500

    More test cleanup.

--- molecule-tests.sh
+++ molecule-tests.sh
@@ -16,3 +16,12 @@ MOLECULE_BOX="w2025_base" molecule test -s wds_boot_image
 MOLECULE_BOX="w2025_base" molecule test -s wds_initialize
 MOLECULE_BOX="w2025_base" molecule test -s win_combine_certificates
 MOLECULE_BOX="w2025_base" molecule test -s win_package_provider
+
+MOLECULE_BOX="w11_24h2_base" molecule test -s win_combine_certificates
+MOLECULE_BOX="w11_24h2_base" molecule test -s win_package_provider
+
+MOLECULE_BOX="w11_23h2_base" molecule test -s win_combine_certificates
+MOLECULE_BOX="w11_23h2_base" molecule test -s win_package_provider
+
+MOLECULE_BOX="w10_22h2_base" molecule test -s win_combine_certificates
+MOLECULE_BOX="w10_22h2_base" molecule test -s win_package_provider
--- roles/install_psgallery/molecule-tests.sh
+++ roles/install_psgallery/molecule-tests.sh
@@ -3,8 +3,10 @@
 set -e
 
 MOLECULE_BOX="w2022_cis" molecule test
-MOLECULE_BOX="w2019_cis" molecule test
 MOLECULE_BOX="w11_24h2_base" molecule test
+
+MOLECULE_BOX="w2019_cis" molecule test
 MOLECULE_BOX="w2025_base" molecule test
+
 MOLECULE_BOX="w11_23h2_base" molecule test
 MOLECULE_BOX="w10_22h2_base" molecule test
